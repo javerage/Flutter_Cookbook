03841dd074ce76f3d6c0bf754a1f8536c53a3047
javerage <joel.chuc@gmail.com>
2022-03-25 02:19:27 -0600

    Move widget at widgets folder

--- Forms/form_validations/lib/screens/custom_form.dart
+++ Forms/form_validations/lib/screens/custom_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_validations/widgets/custom_text_form_field_widget.dart';
 
 class CustomFormScreen extends StatefulWidget {
   const CustomFormScreen({Key? key}) : super(key: key);
@@ -19,21 +20,25 @@ class _CustomFormScreenState extends State<CustomFormScreen> {
             child: Column(
               children: <Widget>[
                 const CustomTextFormFieldWidget(
-                    hintText: 'First Name',
-                    labelText: 'First Name',
-                    keyboardType: TextInputType.name),
+                  hintText: 'First Name',
+                  labelText: 'First Name',
+                  keyboardType: TextInputType.name,
+                ),
                 const CustomTextFormFieldWidget(
-                    hintText: 'Last Name',
-                    labelText: 'Last Name',
-                    keyboardType: TextInputType.name),
+                  hintText: 'Last Name',
+                  labelText: 'Last Name',
+                  keyboardType: TextInputType.name,
+                ),
                 const CustomTextFormFieldWidget(
-                    hintText: 'Phone',
-                    labelText: 'Phone',
-                    keyboardType: TextInputType.phone),
+                  hintText: 'Phone',
+                  labelText: 'Phone',
+                  keyboardType: TextInputType.phone,
+                ),
                 const CustomTextFormFieldWidget(
-                    hintText: 'Email',
-                    labelText: 'Email',
-                    keyboardType: TextInputType.emailAddress),
+                  hintText: 'Email',
+                  labelText: 'Email',
+                  keyboardType: TextInputType.emailAddress,
+                ),
                 const SizedBox(height: 20),
                 Center(
                   child: ElevatedButton(
@@ -60,44 +65,5 @@ class _CustomFormScreenState extends State<CustomFormScreen> {
             ),
           ),
         ));
-  }
-}
-
-class CustomTextFormFieldWidget extends StatelessWidget {
-  final String? labelText;
-  final String? hintText;
-  final TextInputType keyboardType;
-  final TextCapitalization textCapitalization;
-
-  const CustomTextFormFieldWidget({
-    Key? key,
-    this.labelText,
-    this.hintText,
-    this.keyboardType = TextInputType.none,
-    this.textCapitalization = TextCapitalization.words,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: keyboardType,
-      textCapitalization: textCapitalization,
-      decoration: InputDecoration(
-        labelText: labelText,
-        hintText: hintText,
-      ),
-      validator: _validateNullOrEmpty,
-    );
-  }
-
-  String? _validateNullOrEmpty(String? value) {
-    if (value == null) {
-      return 'Please enter some text';
-    }
-
-    if (value.isEmpty) {
-      return 'Please enter some text';
-    }
-    return null;
   }
 }
---
+++ Forms/form_validations/lib/widgets/custom_text_form_field_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormFieldWidget extends StatelessWidget {
+  final String? labelText;
+  final String? hintText;
+  final TextInputType keyboardType;
+  final TextCapitalization textCapitalization;
+
+  const CustomTextFormFieldWidget({
+    Key? key,
+    this.labelText,
+    this.hintText,
+    this.keyboardType = TextInputType.none,
+    this.textCapitalization = TextCapitalization.words,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: keyboardType,
+      textCapitalization: textCapitalization,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+      ),
+      validator: _validateNullOrEmpty,
+    );
+  }
+
+  String? _validateNullOrEmpty(String? value) {
+    if (value == null) {
+      return 'Please enter some text';
+    }
+
+    if (value.isEmpty) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+}
